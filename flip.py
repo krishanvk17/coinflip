a775a41f56627bd462a9956fefc708a4455a3625
krishanvk17 <krishan.vallamkon.7@gmail.com>
2025-03-23 11:28:52 -0500

    Update flip.py

--- flip.py
+++ flip.py
@@ -5,7 +5,7 @@ st.title("Coin flip!")
 st.write("This is Coin Flip, a simple coin flipper, it lands on heads or tails.")
 num=random.randint(1,2)
 if st.button("Flip!"):
-  if num=1:
+  if num==1:
     st.write("Heads!")
   else:
     st.write("Tails!")
