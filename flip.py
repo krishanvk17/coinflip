77fb2cbeca236fc4625d85a9d1c9de8a00c6693d
krishanvk17 <krishan.vallamkon.7@gmail.com>
2025-03-23 11:26:29 -0500

    Create flip.py

---
+++ flip.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import random
+
+st.title("Coin flip!")
+st.write("This is Coin Flip, a simple coin flipper, it lands on heads or tails.")
+num=random.randint(1,2)
+if st.button("Flip!"):
+  if num=1:
+    st.write("Heads!")
+  else:
+    st.write("Tails!")
+
+st.write("refresh to try again!")
+  
